2890830c4793de35b8cdaf4d85c9a173636be88c
ros_user <ros_user@ubuntu-vm.myguest.virtualbox.org>
2023-12-12 10:27:12 +0100

    ez besült

--- ros2_course/turtlesim_controller.py
+++ ros2_course/turtlesim_controller.py
@@ -9,40 +9,25 @@ class TurtlesimController(Node):
 
     def __init__(self):
         super().__init__('turtlesim_controller')
-        self.twist_pub = self.create_publisher(Twist, '/turtle1/cmd_vel', 10)
+
+        self.declare_parameter('speed', 1.0)
+        self.declare_parameter('omega', 20.0)
 
         self.pose = None
         self.subscription = self.create_subscription(
-           Pose,
-           '/turtle1/pose',
-           self.cb_pose,
-           10)
+            Pose,
+            '/turtle1/pose',
+            self.cb_pose,
+            10)
 
-    # New method for TurtlesimController
+        self.twist_pub = self.create_publisher(Twist, '/turtle1/cmd_vel', 10)
+
     def cb_pose(self, msg):
         self.pose = msg
 
-    def go_to(self, speed, omega,y,x):
-        loop_rate = self.create_rate(100, self.get_clock()) #HZ
-        while self.pose is None and rclpy.ok():
-            self.get_logger().info('Waiting for pose...')
-            rclpy.spin_once(self)
 
-        #stuff with atan2
-        x0 = self.pose.x
-        y0 = self.pose.y
-        theta_0 = math.degrees(self.pose.theta)
-
-        theta_1 = math.degrees(math.atan2(y-y0, x-x0))
-        angle = theta_1 - theta_0
-        distance = math.sqrt(((x - x0) * (x - x0)) + (y - y0) * (y - y0))
-
-        # Execute movement
-        self.turn(omega, angle)
-        self.go_straight(speed, distance)
-
-    def go_straight(self, speed, distance):
-        # Create and publish msg
+    def go_straight(self, distance):
+        speed = self.get_parameter('speed').get_parameter_value().double_value
         vel_msg = Twist()
         if distance > 0:
             vel_msg.linear.x = speed
@@ -58,25 +43,26 @@ class TurtlesimController(Node):
         loop_rate = self.create_rate(100, self.get_clock()) # Hz
 
         # Calculate time
-        T = abs(distance / speed)
+        T = abs(distance/speed)     # s
 
-        # Publish first msg and note time when to stop
+        # Publish first msg and note time
+        #self.get_logger().info('Turtle started.')
         self.twist_pub.publish(vel_msg)
-        # self.get_logger().info('Turtle started.')
         when = self.get_clock().now() + rclpy.time.Duration(seconds=T)
 
         # Publish msg while the calculated time is up
-        while (self.get_clock().now() <= when) and rclpy.ok():
+        while (self.get_clock().now() < when) and rclpy.ok():
             self.twist_pub.publish(vel_msg)
-            # self.get_logger().info('On its way...')
+            #self.get_logger().info('On its way...')
             rclpy.spin_once(self)   # loop rate
 
-        # turtle arrived, set velocity to 0
+        # Set velocity to 0
         vel_msg.linear.x = 0.0
         self.twist_pub.publish(vel_msg)
-        # self.get_logger().info('Arrived to destination.')
+        #self.get_logger().info('Arrived to destination.')
 
-    def turn(self, omega, angle):
+    def turn(self, angle):
+        omega = self.get_parameter('omega').get_parameter_value().double_value
         vel_msg = Twist()
 
         vel_msg.linear.x = 0.0
@@ -84,55 +70,78 @@ class TurtlesimController(Node):
         vel_msg.linear.z = 0.0
         vel_msg.angular.x = 0.0
         vel_msg.angular.y = 0.0
-
         if angle > 0:
             vel_msg.angular.z = math.radians(omega)
         else:
-            vel_msg.angular.z= -math.radians(omega)
+            vel_msg.angular.z = math.radians(-omega)
 
-            # Set loop rate
+        # Set loop rate
         loop_rate = self.create_rate(100, self.get_clock()) # Hz
 
         # Calculate time
-        T = abs(angle / omega)
+        T = abs(angle/omega)     # s
 
-        # Publish first msg and note time when to stop
+        # Publish first msg and note time
+        #self.get_logger().info('Turtle started.')
         self.twist_pub.publish(vel_msg)
-        # self.get_logger().info('Turtle started.')
         when = self.get_clock().now() + rclpy.time.Duration(seconds=T)
 
         # Publish msg while the calculated time is up
-        while (self.get_clock().now() <= when) and rclpy.ok():
+        while (self.get_clock().now() < when) and rclpy.ok():
             self.twist_pub.publish(vel_msg)
-            # self.get_logger().info('On its way...')
+            #self.get_logger().info('On its way...')
             rclpy.spin_once(self)   # loop rate
 
-        # turtle arrived, set velocity to 0
+        # Set velocity to 0
         vel_msg.angular.z = 0.0
         self.twist_pub.publish(vel_msg)
-        # self.get_logger().info('Arrived to destination.')
+        #self.get_logger().info('Arrived to destination.')
 
 
-    def draw_poly(self, speed, omega, N, a):
-        angle = 360.0 / N
+    def draw_square(self, a):
+        for i in range(4):
+            self.go_straight(a)
+            self.turn(90)
+
+    def draw_poly(self, N, a):
+        angle = 360 / N
         for i in range(N):
-            self.go_straight(speed, a)
-            self.turn(omega, angle)
+            self.go_straight(a)
+            self.turn(angle)
 
 
+    def go_to(self, x, y):
+        # Set loop rate
+        loop_rate = self.create_rate(100, self.get_clock()) # Hz
+        while self.pose is None and rclpy.ok():
+            self.get_logger().info('Waiting for pose...')
+            rclpy.spin_once(self)
 
+        x0 = self.pose.x
+        y0 = self.pose.y
+        theta_0 = self.pose.theta
+
+        print("Going to (", x, ", ", y, ")")
+        angle = (((math.atan2(y - y0, x - x0) - theta_0) * 180) / math.pi)
+        print("angle ", angle)
+        self.turn(angle)
+
+        distance = math.sqrt(((x- x0) * (x - x0)) + ((y - y0) * (y - y0)))
+        self.go_straight(distance)
+        print("Arrived to (", self.pose.x, ", ", self.pose.y, ")")
 
 def main(args=None):
     rclpy.init(args=args)
     tc = TurtlesimController()
-    #tc.go_straight(-1.0,4.0) #sebesség, távolság
-    #tc.draw_poly(3.0,300.0,6,2) #sebesség, szögsebesség, csúcsok száma, élek száma
+    #tc.turn(20.0,90.0)
+    #tc.go_straight(1.0,4.0)
 
-    tc.go_to(1.0, 20.0, 2, 8)
-    tc.go_to(1.0, 20.0, 2, 2)
-    tc.go_to(1.0, 20.0, 3, 4)
-    tc.go_to(1.0, 20.0, 6, 2)
+    #tc.draw_poly(1.0, 90.0, 6, 3.0)
 
+    tc.go_to(2, 8)
+    tc.go_to(2, 2)
+    tc.go_to(3, 4)
+    tc.go_to(6, 2)
 
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically
