2c1ac5fab789effd0a8a7af1328583782686dbd5
szklipy <92113332+szklipy@users.noreply.github.com>
2023-12-12 10:47:26 +0100

    Update turtlesim_controller.py
    sajna nem működik rendesen

--- ros2_course/turtlesim_controller.py
+++ ros2_course/turtlesim_controller.py
@@ -1,33 +1,26 @@
 import math
 import rclpy
 from rclpy.node import Node
-from geometry_msgs.msg import Twist
-from turtlesim.msg import Pose
-
+from geometry_msgs.msg import Twist, Pose
+from turtlesim.msg import Pose as TurtlesimPose
 
 class TurtlesimController(Node):
 
     def __init__(self):
         super().__init__('turtlesim_controller')
-
-        self.declare_parameter('speed', 1.0)
-        self.declare_parameter('omega', 20.0)
-
+        self.twist_pub = self.create_publisher(Twist, '/turtle1/cmd_vel', 10)
         self.pose = None
         self.subscription = self.create_subscription(
-            Pose,
+            TurtlesimPose,
             '/turtle1/pose',
             self.cb_pose,
-            10)
-
-        self.twist_pub = self.create_publisher(Twist, '/turtle1/cmd_vel', 10)
+            10
+        )
 
     def cb_pose(self, msg):
         self.pose = msg
 
-
-    def go_straight(self, distance):
-        speed = self.get_parameter('speed').get_parameter_value().double_value
+    def go_straight(self, speed, distance):
         vel_msg = Twist()
         if distance > 0:
             vel_msg.linear.x = speed
@@ -39,115 +32,66 @@ class TurtlesimController(Node):
         vel_msg.angular.y = 0.0
         vel_msg.angular.z = 0.0
 
-        # Set loop rate
-        loop_rate = self.create_rate(100, self.get_clock()) # Hz
+        loop_rate = self.create_rate(100, self.get_clock())  # Hz
+        initial_time = self.get_clock().now().nanoseconds
 
-        # Calculate time
-        T = abs(distance/speed)     # s
-
-        # Publish first msg and note time
-        #self.get_logger().info('Turtle started.')
-        self.twist_pub.publish(vel_msg)
-        when = self.get_clock().now() + rclpy.time.Duration(seconds=T)
-
-        # Publish msg while the calculated time is up
-        while (self.get_clock().now() < when) and rclpy.ok():
+        while self.get_clock().now().nanoseconds - initial_time < int(1e9 * (distance / speed)):
             self.twist_pub.publish(vel_msg)
-            #self.get_logger().info('On its way...')
-            rclpy.spin_once(self)   # loop rate
+            rclpy.spin_once(self)
+            loop_rate.sleep()
 
-        # Set velocity to 0
+        # Stop the turtle
         vel_msg.linear.x = 0.0
         self.twist_pub.publish(vel_msg)
-        #self.get_logger().info('Arrived to destination.')
 
-    def turn(self, angle):
-        omega = self.get_parameter('omega').get_parameter_value().double_value
+    def turn(self, omega, angle):
         vel_msg = Twist()
+        vel_msg.angular.z = omega
 
-        vel_msg.linear.x = 0.0
-        vel_msg.linear.y = 0.0
-        vel_msg.linear.z = 0.0
-        vel_msg.angular.x = 0.0
-        vel_msg.angular.y = 0.0
-        if angle > 0:
-            vel_msg.angular.z = math.radians(omega)
-        else:
-            vel_msg.angular.z = math.radians(-omega)
+        loop_rate = self.create_rate(100, self.get_clock())  # Hz
+        initial_time = self.get_clock().now().nanoseconds
 
-        # Set loop rate
-        loop_rate = self.create_rate(100, self.get_clock()) # Hz
-
-        # Calculate time
-        T = abs(angle/omega)     # s
-
-        # Publish first msg and note time
-        #self.get_logger().info('Turtle started.')
-        self.twist_pub.publish(vel_msg)
-        when = self.get_clock().now() + rclpy.time.Duration(seconds=T)
-
-        # Publish msg while the calculated time is up
-        while (self.get_clock().now() < when) and rclpy.ok():
+        while self.get_clock().now().nanoseconds - initial_time < int(1e9 * (angle / omega)):
             self.twist_pub.publish(vel_msg)
-            #self.get_logger().info('On its way...')
-            rclpy.spin_once(self)   # loop rate
+            rclpy.spin_once(self)
+            loop_rate.sleep()
 
-        # Set velocity to 0
+        # Stop the turtle
         vel_msg.angular.z = 0.0
         self.twist_pub.publish(vel_msg)
-        #self.get_logger().info('Arrived to destination.')
 
+    def draw_koch_curve(self, order, size):
+        self.koch_segment(order, size)
 
-    def draw_square(self, a):
-        for i in range(4):
-            self.go_straight(a)
-            self.turn(90)
+    def koch_segment(self, order, size):
+        if order == 0:
+            self.go_straight(1.0, size)
+        else:
+            self.koch_segment(order - 1, size / 3)
+            self.turn(60.0, 0.0)
+            self.koch_segment(order - 1, size / 3)
+            self.turn(-120.0, 0.0)
+            self.koch_segment(order - 1, size / 3)
+            self.turn(60.0, 0.0)
+            self.koch_segment(order - 1, size / 3)
 
-    def draw_poly(self, N, a):
-        angle = 360 / N
-        for i in range(N):
-            self.go_straight(a)
-            self.turn(angle)
+    def destroy(self):
+        self.destroy_node()
+        rclpy.shutdown()
 
-
-    def go_to(self, x, y):
-        # Set loop rate
-        loop_rate = self.create_rate(100, self.get_clock()) # Hz
-        while self.pose is None and rclpy.ok():
-            self.get_logger().info('Waiting for pose...')
-            rclpy.spin_once(self)
-
-        x0 = self.pose.x
-        y0 = self.pose.y
-        theta_0 = self.pose.theta
-
-        print("Going to (", x, ", ", y, ")")
-        angle = (((math.atan2(y - y0, x - x0) - theta_0) * 180) / math.pi)
-        print("angle ", angle)
-        self.turn(angle)
-
-        distance = math.sqrt(((x- x0) * (x - x0)) + ((y - y0) * (y - y0)))
-        self.go_straight(distance)
-        print("Arrived to (", self.pose.x, ", ", self.pose.y, ")")
 
 def main(args=None):
     rclpy.init(args=args)
     tc = TurtlesimController()
-    #tc.turn(20.0,90.0)
-    #tc.go_straight(1.0,4.0)
 
-    #tc.draw_poly(1.0, 90.0, 6, 3.0)
+    # Draw a Koch curve with order=3, size=100
+    tc.draw_koch_curve(order=3, size=100)
 
-    tc.go_to(2, 8)
-    tc.go_to(2, 2)
-    tc.go_to(3, 4)
-    tc.go_to(6, 2)
+    # Uncomment and run the following line to test other movements
+    # tc.draw_poly(3.0, 300.0, 6, 2)  # speed, angular speed, vertices, edges
 
     # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
-    tc.destroy_node()
-    rclpy.shutdown()
+    tc.destroy()
 
 if __name__ == '__main__':
     main()
